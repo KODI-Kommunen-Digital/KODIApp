c6f4e84aaec66e9aed03429253df160f6dbd265c
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-02-15 15:46:37 +0100

    Scraper related changes, cityid bug fix

--- lib/src/presentation/main/home/home_screen/home.dart
+++ lib/src/presentation/main/home/home_screen/home.dart
@@ -217,7 +217,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     showLater: false,
                     shouldPopScope: () => true,
                     canDismissDialog: true,
-                    durationUntilAlertAgain: const Duration(minutes: 30),
+                    durationUntilAlertAgain: const Duration(seconds: 30),
                     dialogStyle: Platform.isIOS
                         ? UpgradeDialogStyle.cupertino
                         : UpgradeDialogStyle.material,
@@ -228,7 +228,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         String? minAppVersion}) {
                       if (display != null) {
                         setState(() {
-                          latestAppStoreVersion = appStoreVersion ?? '5';
+                          latestAppStoreVersion = appStoreVersion ?? '6.1';
                         });
                       }
                     },
