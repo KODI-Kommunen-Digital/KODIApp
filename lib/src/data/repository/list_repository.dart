074498d69c737b07c84830ac55bcf698d80fafc7
DanielFratczak <daniel@heidi-app.de>
2024-03-11 19:16:29 +0100

    Fix searching for all listings

--- lib/src/data/repository/list_repository.dart
+++ lib/src/data/repository/list_repository.dart
@@ -91,7 +91,7 @@ class ListRepository {
     }
     final response = await Api.requestSearchListing(content, linkFilter);
     if (response.success) {
-      final list = List.from(response.data ?? []).map((item) {
+      final list = List<Map<String, dynamic>>.from(response.data ?? []).map((item) {
         return ProductModel.fromJson(item, setting: Application.setting);
       }).toList();
       return [list];
