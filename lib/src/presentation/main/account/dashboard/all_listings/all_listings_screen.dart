335ca70e631b789331fa3427140b1c3601f7ca35
DanielFratczak <daniel@heidi-app.de>
2024-03-20 15:58:58 +0100

    Close dialog bug fix

--- lib/src/presentation/main/account/dashboard/all_listings/all_listings_screen.dart
+++ lib/src/presentation/main/account/dashboard/all_listings/all_listings_screen.dart
@@ -478,48 +478,54 @@ class _AllListingsLoadedState extends State<AllListingsLoaded> {
     String? searchRequest = await showDialog(
       context: context,
       builder: (BuildContext context) {
-        return SimpleDialog(
-            title: Center(
-                child: Text(Translate.of(context).translate('search_title'))),
-            contentPadding: const EdgeInsets.all(24.0),
-            children: [
-              AppTextInput(
-                hintText: Translate.of(context).translate('search_title'),
-                keyboardType: TextInputType.text,
-                controller: _searchController,
-                //focusNode: _focusPass,
-              ),
-              const SizedBox(height: 8.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      _searchController.clear();
-                      Navigator.pop(context, null);
-                    },
-                    child: Text(Translate.of(context).translate('cancel')),
-                  ),
-                  const SizedBox(width: 8.0),
-                  TextButton(
-                    child: Text(
-                      Translate.of(context).translate('search_title'),
+        return WillPopScope(
+          onWillPop: () async {
+            Navigator.pop(context, searchTerm);
+            return false;
+          },
+          child: SimpleDialog(
+              title: Center(
+                  child: Text(Translate.of(context).translate('search_title'))),
+              contentPadding: const EdgeInsets.all(24.0),
+              children: [
+                AppTextInput(
+                  hintText: Translate.of(context).translate('search_title'),
+                  keyboardType: TextInputType.text,
+                  controller: _searchController,
+                  //focusNode: _focusPass,
+                ),
+                const SizedBox(height: 8.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        _searchController.clear();
+                        Navigator.pop(context, null);
+                      },
+                      child: Text(Translate.of(context).translate('cancel')),
                     ),
-                    onPressed: () {
-                      String content = _searchController.text;
-                      Navigator.pop(context, content);
-                    },
-                  ),
-                ],
-              ),
-            ]);
+                    const SizedBox(width: 8.0),
+                    TextButton(
+                      child: Text(
+                        Translate.of(context).translate('search_title'),
+                      ),
+                      onPressed: () {
+                        String content = _searchController.text;
+                        Navigator.pop(context, content);
+                      },
+                    ),
+                  ],
+                ),
+              ]),
+        );
       },
     );
     return searchRequest;
   }
 
   Future _searchListings() async {
-    dynamic searchResult = await openSearchDialog();
+    String? searchResult = await openSearchDialog();
 
     if (searchResult is String && searchResult.trim() != "") {
       pageNo = 1;
