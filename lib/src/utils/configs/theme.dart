89c258703367568a696b5b5bc733834d1b201059
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-04-05 12:17:44 +0200

    Color change

--- lib/src/utils/configs/theme.dart
+++ lib/src/utils/configs/theme.dart
@@ -20,7 +20,7 @@ class AppTheme {
   ///Default Theme
   static final ThemeModel defaultTheme = ThemeModel.fromJson({
     "name": "default",
-    "primary": '#2c99ff',
+    "primary": '#fecc00',
     "secondary": "#FFFFFF",
   });
 
@@ -28,7 +28,7 @@ class AppTheme {
   static final List themeSupport = [
     {
       "name": "default",
-      "primary": '#2c99ff',
+      "primary": '#fecc00',
       "secondary": "#FFFFFF",
     },
     {
