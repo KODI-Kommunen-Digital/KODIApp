2d83c085dd9e7e24d193757a31f909e4d5264158
DanielFratczak <daniel@heidi-app.com>
2024-05-17 14:44:44 +0200

    Remove location filter

--- lib/src/presentation/main/home/filter_screen.dart
+++ lib/src/presentation/main/home/filter_screen.dart
@@ -23,7 +23,6 @@ class _FilterScreenState extends State<FilterScreen> {
   @override
   void initState() {
     super.initState();
-    currentCity = widget.multiFilter.currentLocation;
     currentCategory = widget.multiFilter.currentCategory;
     currentProductEventFilter = widget.multiFilter.currentProductEventFilter;
     currentListingStatus = widget.multiFilter.currentListingStatus;
@@ -44,7 +43,6 @@ class _FilterScreenState extends State<FilterScreen> {
             Navigator.pop(
                 context,
                 MultiFilter(
-                  currentLocation: currentCity,
                   currentProductEventFilter: currentProductEventFilter,
                   currentListingStatus: currentListingStatus,
                   currentForumGroupFilter: currentForumGroupFilter,
@@ -52,7 +50,6 @@ class _FilterScreenState extends State<FilterScreen> {
                   hasForumGroupFilter: widget.multiFilter.hasForumGroupFilter,
                   hasProductEventFilter:
                       widget.multiFilter.hasProductEventFilter,
-                  hasLocationFilter: widget.multiFilter.hasLocationFilter,
                   hasListingStatusFilter:
                       widget.multiFilter.hasListingStatusFilter,
                   hasCategoryFilter: widget.multiFilter.hasCategoryFilter
@@ -61,8 +58,6 @@ class _FilterScreenState extends State<FilterScreen> {
           },
           child: Column(
             children: [
-              if (widget.multiFilter.hasLocationFilter == true)
-                ..._buildLocationFilter(),
               if (widget.multiFilter.hasProductEventFilter == true)
                 ..._buildProductEventFilter(),
               if (widget.multiFilter.hasListingStatusFilter == true)
@@ -76,47 +71,6 @@ class _FilterScreenState extends State<FilterScreen> {
         ),
       ),
     ));
-  }
-
-  List<Widget> _buildLocationFilter() {
-    return [
-      const SizedBox(
-        height: 8,
-      ),
-      Center(
-          child: Text(
-        Translate.of(context).translate('choose_city'),
-        style: Theme.of(context)
-            .textTheme
-            .titleMedium!
-            .copyWith(fontWeight: FontWeight.bold),
-      )),
-      Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Wrap(spacing: 8.0, children: [
-          ChoiceChip(
-            label: Text(Translate.of(context).translate('select_location')),
-            selected: 0 == currentCity,
-            onSelected: (selected) {
-              setState(() {
-                currentCity = 0;
-              });
-            },
-          ),
-          ...widget.multiFilter.cities!.map((city) {
-            return ChoiceChip(
-              label: Text(city.title),
-              selected: city.id == currentCity,
-              onSelected: (selected) {
-                setState(() {
-                  currentCity = city.id;
-                });
-              },
-            );
-          }).toList(),
-        ]),
-      )
-    ];
   }
 
   List<Widget> _buildListingStatusFilter() {
