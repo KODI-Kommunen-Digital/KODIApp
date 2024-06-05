b222e1b0438e4d4880ce3f752e76d81235320e1b
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-06-05 14:46:51 +0200

    Final changes

--- lib/src/presentation/main/home/widget/home_swiper.dart
+++ lib/src/presentation/main/home/widget/home_swiper.dart
@@ -22,7 +22,7 @@ class HomeSwipe extends StatelessWidget {
       return Swiper(
           itemBuilder: (BuildContext context, int index) {
             return CachedNetworkImage(
-              imageUrl: "${Application.picturesURL}admin/Homepage.jpg",
+              imageUrl: "${Application.picturesURL}admin/Homepage1.jpg",
               cacheManager: memoryCacheManager,
               placeholder: (context, url) {
                 return AppPlaceholder(
@@ -38,7 +38,7 @@ class HomeSwipe extends StatelessWidget {
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: imageProvider,
-                      fit: BoxFit.cover,
+                      fit: BoxFit.fill,
                     ),
                   ),
                 );
