a00a808bd5ed54e2bc169e4967455879f11b564d
DanielFratczak <daniel@heidi-app.de>
2024-06-10 11:55:57 +0200

    Fix empty user

--- lib/src/presentation/main/account/account_profile/account_screen.dart
+++ lib/src/presentation/main/account/account_profile/account_screen.dart
@@ -126,7 +126,7 @@ class _AccountLoadedState extends State<AccountLoaded> {
                       type: UserViewType.information,
                       onPressed: () {
                         Navigator.pushNamed(context, Routes.profile,
-                            arguments: {'editable': true});
+                            arguments: {'editable': true, 'user': user});
                       },
                       showDirectionIcon: true,
                     ),
