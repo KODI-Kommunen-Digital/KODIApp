895825170ad58027c5c43d85431f0a6954424a47
DanielFratczak <daniel@heidi-app.com>
2024-07-17 12:29:03 +0200

    Create null check

--- lib/src/presentation/main/account/dashboard/my_listings/my_listings_screen.dart
+++ lib/src/presentation/main/account/dashboard/my_listings/my_listings_screen.dart
@@ -251,18 +251,21 @@ class _ProfileLoadedState extends State<ProfileLoaded> {
                                                                               .bold,
                                                                     ),
                                                               ),
-                                                              Text(
-                                                                '${Translate.of(context).translate('views')}: ${userListingsList[index].viewCount ?? 0}',
-                                                                style: Theme.of(
-                                                                        context)
-                                                                    .textTheme
-                                                                    .bodySmall!
-                                                                    .copyWith(
-                                                                      fontWeight:
-                                                                          FontWeight
-                                                                              .w100,
-                                                                    ),
-                                                              ),
+                                                              if (userListingsList[
+                                                                          index]
+                                                                      .viewCount !=
+                                                                  null)
+                                                                Text(
+                                                                  '${Translate.of(context).translate('views')}: ${userListingsList[index].viewCount}',
+                                                                  style: Theme.of(
+                                                                          context)
+                                                                      .textTheme
+                                                                      .bodySmall!
+                                                                      .copyWith(
+                                                                        fontWeight:
+                                                                            FontWeight.w100,
+                                                                      ),
+                                                                ),
                                                             ],
                                                           ),
                                                           const SizedBox(
