2ba713afc7735bbfac14c775b1e472f010708b78
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-09-03 13:58:59 +0200

    Changes

--- lib/src/presentation/main/home/home_screen/home.dart
+++ lib/src/presentation/main/home/home_screen/home.dart
@@ -307,6 +307,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       }),
                   pinned: true,
                 ),
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 25,
+                  ),
+                ),
                 CupertinoSliverRefreshControl(
                   onRefresh: _onRefresh,
                 ),
