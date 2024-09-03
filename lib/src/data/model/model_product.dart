3c4f04debd4b3f0a0f54b289cfbe2b615862b2bd
DanielFratczak <daniel@heidi-app.de>
2024-09-03 13:35:52 +0200

    Listing image ecmaps fetcher

--- lib/src/data/model/model_product.dart
+++ lib/src/data/model/model_product.dart
@@ -158,6 +158,8 @@ class ProductModel {
     String priceMax = '';
     String priceDisplay = '';
     int? timeless;
+    String imageUrl =
+        'admin/News/Defaultimage${json['id'].toString().substring(json['id'].toString().length - 1)}.png';
 
     if (json['author'] != null) {
       author = UserModel.fromJson(json['author']);
@@ -236,14 +238,37 @@ class ProductModel {
       priceDisplay = json['booking_price_display'];
     }
 
+    if (json['logo'] != null) {
+      var image = json['logo'];
+      if (image is List<dynamic> && json['logo'].isNotEmpty) {
+        image = json['logo'][0]?.toString();
+      } else {
+        image = json['logo']?.toString();
+      }
+      bool isEcmapsDomain = image.startsWith('img.ecmaps.de/remote/.jpg?');
+      bool isNaN = image ==
+          'https://gesekeheidi.obs.eu-de.otc.t-systems.com/admin/eatOrDrink/DefaultimageNaN.png';
+
+      if (isEcmapsDomain) {
+        final urlRegex = RegExp(r"https.*\.(jpg|png)");
+        final match = urlRegex.firstMatch(image);
+        final extractedUrl = match?.group(0);
+        if (extractedUrl != null) {
+          final parsedUrl =
+          extractedUrl.replaceAll("%3A", ":").replaceAll("%2F", "/");
+          imageUrl = parsedUrl;
+        }
+      } else if (!isNaN) {
+        imageUrl = image;
+      }
+    }
+
     return ProductModel(
       id: json['id'],
       userId: json['userId'] ?? 0,
       title: json['title'] ?? '',
       timeless: timeless,
-      image: (json['logo'] is List<dynamic> && json['logo'].isNotEmpty)
-          ? json['logo'][0]?.toString() ?? 'admin/News.jpeg'
-          : (json['logo']?.toString() ?? 'admin/News.jpeg'),
+      image: imageUrl,
       videoURL: videoURL,
       category: category ?? '',
       createDate: createDate,
@@ -380,10 +405,36 @@ class ImageListModel {
   ImageListModel({this.id, this.imageOrder, this.listingId, this.logo});
 
   ImageListModel.fromJson(Map<String, dynamic> json) {
+    String imageUrl = '';
+
+    if (json['logo'] != null) {
+      var image = json['logo'];
+      if (image is List<dynamic> && json['logo'].isNotEmpty) {
+        image = json['logo'][0]?.toString();
+      } else {
+        image = json['logo']?.toString();
+      }
+      bool isEcmapsDomain = image.startsWith('img.ecmaps.de/remote/.jpg?');
+      bool isNaN = image ==
+          'https://gesekeheidi.obs.eu-de.otc.t-systems.com/admin/eatOrDrink/DefaultimageNaN.png';
+
+      if (isEcmapsDomain) {
+        final urlRegex = RegExp(r"https.*\.(jpg|png)");
+        final match = urlRegex.firstMatch(image);
+        final extractedUrl = match?.group(0);
+        if (extractedUrl != null) {
+          final parsedUrl =
+          extractedUrl.replaceAll("%3A", ":").replaceAll("%2F", "/");
+          imageUrl = parsedUrl;
+        }
+      } else if (!isNaN) {
+        imageUrl = image;
+      }
+    }
     id = json['id'];
     imageOrder = json['imageOrder'];
     listingId = json['listingId'];
-    logo = json['logo'];
+    logo = imageUrl;
   }
 
   Map<String, dynamic> toJson() {
