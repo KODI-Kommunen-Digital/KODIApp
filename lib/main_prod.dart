7ee84d8ce13cbae31b8c46e2563aa0b17ac199c8
DanielFratczak <daniel@heidi-app.de>
2024-09-17 13:43:27 +0200

    Connect to Sentry

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -52,7 +52,7 @@ Future<void> main() async {
 
   await SentryFlutter.init((options) {
     options.dsn =
-        'https://a4fb5224118623425d802bf0acaf087b@o4506393481510912.ingest.sentry.io/4506393482493952';
+        'https://a6a88ea3f5f3d8e45c7743bfc9af1cad@o4507264812908544.ingest.de.sentry.io/4507968022184016';
     options.tracesSampleRate = 0.01;
   }, appRunner: () => runApp(HeidiApp(prefBox)));
   await dotenv.load(fileName: "assets/env/.envTroisdorf");
