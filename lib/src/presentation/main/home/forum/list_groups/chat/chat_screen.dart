e26d36fef99dd26cf1ecaa73a0b24dd18ebac377
DenioSJ <denioshaji28@gmail.com>
2024-10-28 12:32:35 +0100

    Apk Changes

--- lib/src/presentation/main/home/forum/list_groups/chat/chat_screen.dart
+++ lib/src/presentation/main/home/forum/list_groups/chat/chat_screen.dart
@@ -129,7 +129,7 @@ class _ChatLoadedState extends State<ChatLoaded> {
   }
 
   Future<void> _connectWebsocket(int? cityId, int? forumId) async {
-    final wsUrl = Uri.parse('ws://5.75.161.217:3040/ws');
+    final wsUrl = Uri.parse('wss://appcms.mitwitz.de/websocket/ws');
     channel = WebSocketChannel.connect(wsUrl);
     await channel?.ready;
     channel?.sink.add(jsonEncode({
