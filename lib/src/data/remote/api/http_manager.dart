69556eba01944cd39a6c47f9488be447ae72f53d
DenioSJ <denioshaji28@gmail.com>
2025-01-21 16:18:28 +0100

    Base url change

--- lib/src/data/remote/api/http_manager.dart
+++ lib/src/data/remote/api/http_manager.dart
@@ -16,8 +16,9 @@ class HTTPManager {
   late String _baseUrl;
 
   HTTPManager({bool forum = false}) {
-    _baseUrl =
-        !forum ? 'http://www.63inside.de/api/' : 'http://www.63inside.de/api/';
+    _baseUrl = !forum
+        ? 'http://www.63inside.de/api/v2/'
+        : 'http://www.63inside.de/api/';
 
     _dio = Dio(
       BaseOptions(
