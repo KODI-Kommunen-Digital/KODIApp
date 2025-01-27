89d18d6f99fc93a0115cf0096fe456c1cbf50ad6
DenioSJ <denioshaji28@gmail.com>
2025-01-27 10:35:59 +0100

    IOS release update changes

--- lib/src/presentation/main/home/home_screen/home.dart
+++ lib/src/presentation/main/home/home_screen/home.dart
@@ -299,7 +299,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         String? minAppVersion}) {
                       if (display != null) {
                         setState(() {
-                          latestAppStoreVersion = appStoreVersion ?? '1.0.3';
+                          latestAppStoreVersion = appStoreVersion ?? '1.0.4';
                         });
                       }
                     },
