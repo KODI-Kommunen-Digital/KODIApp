12092f89f74969afe0227458a541baee23150712
DanielFratczak <daniel@heidi-app.com>
2025-02-27 10:30:58 +0100

    Sentry change dns

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -53,7 +53,7 @@ Future<void> main() async {
 
   await SentryFlutter.init((options) {
     options.dsn =
-        'https://196f54a62defd6f94e81653c8b98b17d@o4506393481510912.ingest.us.sentry.io/4508885909045248';
+        'https://667175baae08bf96e43d0b5b5444deeb@o4507264812908544.ingest.de.sentry.io/4508890460323920';
     options.tracesSampleRate = 0.01;
   }, appRunner: () => runApp(SentryWidget(child: HeidiApp(prefBox))));
   await dotenv.load(fileName: "assets/env/.envRatingen");
