4b86371366662b41f1c2851822183981c64439b1
DanielFratczak <daniel@heidi-app.com>
2025-03-04 15:26:25 +0100

    Event filter bug fix

--- lib/src/presentation/main/home/list_product/list_product.dart
+++ lib/src/presentation/main/home/list_product/list_product.dart
@@ -48,9 +48,10 @@ class _ListProductScreenState extends State<ListProductScreen> {
 
   MultiFilter whatCanFilter(bool isEvent) {
     if (isEvent) {
+      print(selectedFilter?.currentProductEventFilter);
       return MultiFilter(
         hasProductEventFilter: true,
-        currentProductEventFilter: selectedFilter?.currentProductEventFilter,
+        currentProductEventFilter: selectedFilter?.currentProductEventFilter ?? context.read<ListCubit>().eventFilter,
       );
     } else {
       return MultiFilter();
