cb47ccc11272cfecc34bc6622678228f7020db9b
DanielFratczak <daniel@heidi-app.com>
2025-04-08 14:38:55 +0200

    Fix news images not loading

--- lib/src/data/model/model_product.dart
+++ lib/src/data/model/model_product.dart
@@ -307,7 +307,7 @@ class ProductModel {
           imageUrl = parsedUrl;
         }
       } else if (!isNaN) {
-        imageUrl = image;
+        imageUrl = image.replaceAll('https', 'http');
       }
     }
 
