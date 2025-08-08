26fa526624e5f2df87925819122641e070ed68bb
DenioSJ <denioshaji28@gmail.com>
2025-08-08 19:03:49 +0200

    Email change

--- lib/src/presentation/main/account/contact_us/contact_us_screen.dart
+++ lib/src/presentation/main/account/contact_us/contact_us_screen.dart
@@ -80,7 +80,7 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
                       borderRadius: BorderRadius.circular(10.0),
                     ),
                     child: const Text(
-                      ' ',
+                      'smartcity@gera.de',
                       style: TextStyle(color: Colors.white, fontSize: 17),
                     ),
                   ),
