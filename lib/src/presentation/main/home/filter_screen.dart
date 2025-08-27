a9df0b834f6aa12603ea93449f892a267dba8b46
dev6 <dev6@heidi-app.com>
2025-08-27 19:32:35 +0530

    Refactor: Include external listings in search results
    The `requestSearchListing` API call now includes the `showExternalListings=true` parameter to fetch external listings alongside internal ones.

--- lib/src/presentation/main/home/filter_screen.dart
+++ lib/src/presentation/main/home/filter_screen.dart
@@ -189,33 +189,34 @@ class _FilterScreenState extends State<FilterScreen> {
                     });
                   },
                 ),
-          ...widget.multiFilter.cities!.map((city) {
-            return (widget.multiFilter.hasMultipleCityFilter)
-                ? ChoiceChip(
-                    label: Text(city.title),
-                    selected: currentCities.contains(city.id),
-                    onSelected: (selected) {
-                      setState(() {
-
-                        if (currentCities.contains(city.id)) {
-                          currentCities.remove(city.id);
-                        } else {
-                          currentCities.add(city.id);
-                          currentCities.remove(0);
-                        }
-                      });
-                    },
-                  )
-                : ChoiceChip(
-                    label: Text(city.title),
-                    selected: city.id == currentCity,
-                    onSelected: (selected) {
-                      setState(() {
-                        currentCity = city.id;
-                      });
-                    },
-                  );
-          }),
+          ///Todo uncomment when scraper issue fix
+          // ...widget.multiFilter.cities!.map((city) {
+          //   return (widget.multiFilter.hasMultipleCityFilter)
+          //       ? ChoiceChip(
+          //           label: Text(city.title),
+          //           selected: currentCities.contains(city.id),
+          //           onSelected: (selected) {
+          //             setState(() {
+          //
+          //               if (currentCities.contains(city.id)) {
+          //                 currentCities.remove(city.id);
+          //               } else {
+          //                 currentCities.add(city.id);
+          //                 currentCities.remove(0);
+          //               }
+          //             });
+          //           },
+          //         )
+          //       : ChoiceChip(
+          //           label: Text(city.title),
+          //           selected: city.id == currentCity,
+          //           onSelected: (selected) {
+          //             setState(() {
+          //               currentCity = city.id;
+          //             });
+          //           },
+          //         );
+          // }),
         ]),
       )
     ];
