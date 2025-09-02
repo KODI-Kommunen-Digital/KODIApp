d3081af5955a559334303b49e0b480d43847785a
Naman55 <naman.khanna@fiftyfivetech.io>
2025-09-02 17:03:12 +0530

    update flutter version to 3.32.0

--- lib/src/utils/configs/theme.dart
+++ lib/src/utils/configs/theme.dart
@@ -116,7 +116,7 @@ class AppTheme {
       dividerTheme: const DividerThemeData(
         thickness: 0.8,
       ),
-      bottomAppBarTheme: const BottomAppBarThemeData(
+      bottomAppBarTheme: const BottomAppBarTheme(
         shape: CircularNotchedRectangle(),
       ),
       colorScheme: colorScheme
