fb6402438308e930b0ec432264edea2a4ac43fbb
dev7 <dev7@heidi-app.com>
2025-10-16 18:16:43 +0530

    updated banner text position

--- lib/src/presentation/main/home/widget/banner_slider.dart
+++ lib/src/presentation/main/home/widget/banner_slider.dart
@@ -10,11 +10,9 @@ class BannerSlider extends StatefulWidget {
 }
 
 class _BannerSliderState extends State<BannerSlider> {
-
-  final String imageUrl =
-    "assets/images/home/banner.jpg";
+  final String imageUrl = "assets/images/home/banner.jpg";
   final String title =
-    "Suchst du nach einem Job? Oder suchst du Mitarbeiter:innen? Dann registriere dich jetzt und nutze unser Job-Matching!";
+      "Suchst du nach einem Job? Oder suchst du Mitarbeiter:innen? Dann registriere dich jetzt und nutze unser Job-Matching!";
 
   @override
   void initState() {
@@ -36,37 +34,65 @@ class _BannerSliderState extends State<BannerSlider> {
         children: [
           Text(
             Translate.of(context).translate('apply_for_our_region'),
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(fontWeight: FontWeight.bold),
+            style: Theme.of(
+              context,
+            ).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 10),
-          SizedBox(
-            height: 200,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: GestureDetector(
-                onTap: (){
-                  Utils().showAlertMessage('this_feature_will_be_available_soon', context);
-                },
-                child: RepaintBoundary(
-                  child: Image.asset(imageUrl,
-                    fit: BoxFit.cover,
-                    cacheWidth: 400,
-                    cacheHeight: 200,
-                    width: double.infinity,),
+          Stack(
+            children: [
+              SizedBox(
+                height: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: GestureDetector(
+                    onTap: () {
+                      Utils().showAlertMessage(
+                        'this_feature_will_be_available_soon',
+                        context,
+                      );
+                    },
+                    child: RepaintBoundary(
+                      child: Image.asset(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                        cacheWidth: 400,
+                        cacheHeight: 200,
+                        width: double.infinity,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 4,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      title,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 4,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 10),
-          Center(child: Text(
-            title,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 4,
-          )),
-          const SizedBox(height: 8),
+          // SizedBox(height: 10),
+          // const SizedBox(height: 8),
         ],
       ),
     );
