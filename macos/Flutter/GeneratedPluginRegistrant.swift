643cd41a495f05377864804ccc5f96c47997505c
dev7 <dev7@heidi-app.com>
2026-03-02 19:11:57 +0530

    Update versioning in Info.plist and clean up project configurations.
    - `ios/Runner.xcodeproj/project.pbxproj`: Remove empty input and output paths from CocoaPods build phases.
    - `ios/Runner/Info.plist`: Hardcode version to 5.0.1 and build number to 22.
    - `macos/Flutter/GeneratedPluginRegistrant.swift`: Remove `PathProviderPlugin` registration.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,7 +18,6 @@ import geolocator_apple
 import just_audio
 import location
 import package_info_plus
-import path_provider_foundation
 import sentry_flutter
 import shared_preferences_foundation
 import sqflite_darwin
@@ -41,7 +40,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
